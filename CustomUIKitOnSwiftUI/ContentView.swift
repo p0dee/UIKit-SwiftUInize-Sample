86d93998d63aa4abb5f7b801da7b343a55f553cb
Takeshi Tanaka <takeshi.tanaka@goodpatch.com>
2021-12-08 10:45:19 +0900

    Initial Commit

---
+++ CustomUIKitOnSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CustomUIKitOnSwiftUI
+//
+//  Created by Takeshi Tanaka on 2021/12/08.
+//  Copyright © 2021 Goodpatch. All rights reserved.
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomUIKitOnSwiftUI/CustomUIKitOnSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  CustomUIKitOnSwiftUIApp.swift
+//  CustomUIKitOnSwiftUI
+//
+//  Created by Takeshi Tanaka on 2021/12/08.
+//  Copyright © 2021 Goodpatch. All rights reserved.
+    
+
+import SwiftUI
+
+@main
+struct CustomUIKitOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
